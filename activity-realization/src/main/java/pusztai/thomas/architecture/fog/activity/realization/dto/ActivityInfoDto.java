75cc103f65453ff387328c058bd12595030a1fa2
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2019-03-18 17:50:29 +0100

    Fix activity deserialization

--- activity-realization/src/main/java/pusztai/thomas/architecture/fog/activity/realization/dto/ActivityInfoDto.java
+++ activity-realization/src/main/java/pusztai/thomas/architecture/fog/activity/realization/dto/ActivityInfoDto.java
@@ -5,13 +5,17 @@ import pusztai.thomas.architecture.fog.activity.realization.util.ResourceHelper;
 
 public class ActivityInfoDto {
 
+	private static class ActivityInfoDtoContainer {
+		public ActivityInfoDto activity;
+	}
+
 	public String name;
 	public String initialNode;
 	public ActivityNodeInfoDto[] nodes;
 
 	public static ActivityInfoDto loadFromResource(String resourceName) {
 		String json = ResourceHelper.readResourceAsString(resourceName);
-		return JsonHelper.parseJson(json, ActivityInfoDto.class);
+		return JsonHelper.parseJson(json, ActivityInfoDtoContainer.class).activity;
 	}
 
 }
